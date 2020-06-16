0274dca1dcbb7293d394f7075dcbc1aae6bd1244
castiellian <51224589+castiellian@users.noreply.github.com>
2020-06-16 19:50:07 +0800

    Update Build_OP_3800.sh

--- Build_OP_3800.sh
+++ Build_OP_3800.sh
@@ -17,6 +17,7 @@ git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 git clone https://github.com/kang-mk/luci-app-smartinfo package/luci-app-smartinfo
+git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 
 
 
