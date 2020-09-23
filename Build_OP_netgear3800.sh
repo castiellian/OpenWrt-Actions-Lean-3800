65c38d50be9f51c1585531f2a68024bbda3490ce
castiellian <51224589+castiellian@users.noreply.github.com>
2020-09-23 22:14:57 +0800

    Update Build_OP_netgear3800.sh

--- Build_OP_netgear3800.sh
+++ Build_OP_netgear3800.sh
@@ -7,11 +7,12 @@
 #=================================================
 
 # 定制默认IP
-sed -i 's/192.168.1.1/192.168.1.8/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 
-# 替换默认Argon主题
+
+# 删除原主题
 rm -rf package/lean/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 
 # 添加第三方软件包
 git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
